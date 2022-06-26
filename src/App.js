2757f7383bc66c20d2437d6cc626b0ec2c99f2f3
zelitosaide <zelitosaide83@gmail.com>
2022-06-26 11:37:34 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from 'framer-motion'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <motion.div
+      style={app}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
+      Framer Motion docs
+    </motion.div>
+  )
 }
 
-export default App;
+export default App
+
+const app = {
+  padding: '1rem',
+  background: 'red'
+}
